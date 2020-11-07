2fdf29e85eff3d7ae63505a0858df28c45a6627f
Miguel Magalhães Lopes <miguelzinhorama@gmail.com>
2020-11-07 18:43:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os, subprocess, re
 # config_file = os.environ['RCLONE_CONFIG_FILE']
 config = "./rclone.conf"
 # print(rc.RClone(config_path="./rclone.conf",binary_path="rclone.exe").about())
-cmd = ["rclone.exe", "listremotes", "--config=" + config, "--long"]
+cmd = ["rclone", "listremotes", "--config=" + config, "--long"]
 output = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
 output = re.compile("b\'(.*)\'").findall(str(output))[0].split("\\n")[0:-1]
 for i in range(0, len(output)):
