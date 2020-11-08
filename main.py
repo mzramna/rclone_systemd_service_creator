abba545ac284d0f96669759c47a186cf6f571f1f
miguel Lopes <mzramnadev@gmail.com>
2020-11-08 08:36:34 -0300

    atualizado diretório base para um padrão unificado,atualizavel via script

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import os, subprocess, re
 
 # config_file = os.environ['RCLONE_CONFIG_FILE']
 config = "./rclone.conf"
+rclone_mount_folder="/media/rclone/"
 # print(rc.RClone(config_path="./rclone.conf",binary_path="rclone.exe").about())
-cmd = ["rclone.exe", "listremotes", "--config=" + config, "--long"]
+cmd = ["rclone", "listremotes", "--config=" + config, "--long"]
 output = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
 output = re.compile("b\'(.*)\'").findall(str(output))[0].split("\\n")[0:-1]
 for i in range(0, len(output)):
@@ -16,4 +17,4 @@ template = open("./default_template_service.service").read()
 print(template)
 for i in output:
     service = open("./"+i[0] + ".service", "a")
-    service.write(template.replace("{{remote_name}}", i[0]).replace("{{config_dir}}", config))
+    service.write(template.replace("{{remote_name}}", i[0]).replace("{{config_dir}}", config).replace("{{rclone_folder}}",rclone_mount_folder))
