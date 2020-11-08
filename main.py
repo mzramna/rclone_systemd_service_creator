05ad0d987b3f35ec349c250ae9878be66b6d4439
miguel Lopes <mzramnadev@gmail.com>
2020-11-08 08:38:58 -0300

    reformat

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
-import os, subprocess, re
+import re
+import subprocess
 
 # config_file = os.environ['RCLONE_CONFIG_FILE']
 config = "./rclone.conf"
-rclone_mount_folder="/media/rclone/"
+rclone_mount_folder = "/media/rclone/"
 # print(rc.RClone(config_path="./rclone.conf",binary_path="rclone.exe").about())
 cmd = ["rclone", "listremotes", "--config=" + config, "--long"]
 output = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
@@ -16,5 +17,7 @@ print(output)
 template = open("./default_template_service.service").read()
 print(template)
 for i in output:
-    service = open("./"+i[0] + ".service", "a")
-    service.write(template.replace("{{remote_name}}", i[0]).replace("{{config_dir}}", config).replace("{{rclone_folder}}",rclone_mount_folder))
+    service = open("./" + i[0] + ".service", "a")
+    service.write(
+        template.replace("{{remote_name}}", i[0]).replace("{{config_dir}}", config).replace("{{rclone_folder}}",
+                                                                                            rclone_mount_folder))
